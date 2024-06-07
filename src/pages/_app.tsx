a9abd73224f7e0ec7180f183e4ca8c83e04f1a84
John Anders <ja.invent@gmail.com>
2024-06-07 17:08:48 +0800

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { GeistSans } from "geist/font/sans";
-import { type AppType } from "next/app";
+import { GeistSans } from "geist/font/sans"
+import { type AppType } from "next/app"
 
-import "~/styles/globals.css";
+import "~/styles/globals.css"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={GeistSans.className}>
       <Component {...pageProps} />
     </main>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
