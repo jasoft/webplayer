f7f52a9613bc3dd5730881391a11280ec756b99e
John Anders <ja.invent@gmail.com>
2024-06-08 11:02:10 +0800

    not displaying bot on dev docker

--- src/components/chatbot.jsx
+++ src/components/chatbot.jsx
@@ -1,7 +1,7 @@
-export default () => {
+export function ChatBot() {
   return (
     <iframe
-      src="https://www.coze.cn/store/bot/7377832687211626531?bid=6cos3hp189g18&panel=1"
+      src="https://www.coze.cn/store/bot/7377832687211626531?bid=6cos3hp189g18&panel=0"
       style={{ width: "100%", height: "100%", minHeight: "400px" }}
       frameBorder="0"
       allow="microphone"
